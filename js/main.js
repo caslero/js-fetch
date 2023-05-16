2832de6910cbc8e26ec5c23af0eef4f0d65bf4d1
carlo <carlosjperazab@gmail.com>
2023-05-16 19:05:07 -0400

    Mostrando solo parte de los datos

--- js/main.js
+++ js/main.js
@@ -20,19 +20,18 @@ const infoComic = async() => {
             nombreC = nombreC + informacion.name + "<br><br>";
             estado = estado + informacion.status + "<br><br>";
             especies = especies + informacion.species + "<br><br>";
-            episodio = episodio + informacion.episode + "<br><br>";
-            img = img + informacion.image + "<br><br>";
-            creado = creado + informacion.created + "<br><br>";
+            episodio = episodio + informacion.id + " Cap" + "<br><br>";
+            //img = img + informacion.image + "<br><br>";
+            //creado = creado + informacion.created + "<br><br>";
             
         });
-        
       
         document.getElementById('titulo').innerHTML = nombreC;
         document.getElementById('estado').innerHTML = estado;
         document.getElementById('especies').innerHTML = especies;
         document.getElementById('episodio').innerHTML = episodio;
-        document.getElementById('imagen').innerHTML = img;
-        document.getElementById('creado').innerHTML = creado;
+        //document.getElementById('imagen').innerHTML = img;
+        //document.getElementById('creado').innerHTML = creado;
         
     
     } else if (respuesta.status === 401) {
