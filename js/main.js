c4645e97c8d39df357d71d6b894c7a7eac6f26da
carlo <carlosjperazab@gmail.com>
2023-05-19 14:39:10 -0400

    Aqui se mostramos la candidad de episodios en los que estan cada personaje

--- js/main.js
+++ js/main.js
@@ -4,32 +4,46 @@ const infoComic = async() => {
     try {
         const respuesta = await fetch("https://rickandmortyapi.com/api/character");
 
-        console.log(respuesta);      
+        //console.log(respuesta);      
 
 
     if (respuesta.status === 200) {
-        const datos = await respuesta.json();
+        const { results } = await respuesta.json();
 
         let nombreC = '';
         let estado = '';
         let especies = '';
-        let episodio = '';
+        const episodio = [];
+        let id = '';
         let img = '';
         let creado = '';
-        datos.results.forEach(informacion => {
-            nombreC = nombreC + informacion.name + "<br><br>";
-            estado = estado + informacion.status + "<br><br>";
-            especies = especies + informacion.species + "<br><br>";
-            episodio = episodio + informacion.id + " Cap" + "<br><br>";
-            //img = img + informacion.image + "<br><br>";
-            //creado = creado + informacion.created + "<br><br>";
-            
+
+
+        // iteración con el foreach
+        results.forEach((informacion) =>  {
+            nombreC = nombreC + informacion.name + '<br>';
+            estado = estado + informacion.status + '<br><br>';
+            especies = especies + informacion.species + '<br><br>';
+            episodio.push(informacion.episode.length);
         });
-      
+
+
+         // iteración con un for in
+         //for (let i in results) {
+            // console.log(results[i].name);
+            //nombreC = nombreC + results[i].name + '<br>';
+            //estado = estado + results[i].status + '<br><br>';
+            //especies = especies + results[i].species + '<br><br>';
+            //episodios.push(results[i].episode.length);
+        //}
+
+
+        
         document.getElementById('titulo').innerHTML = nombreC;
         document.getElementById('estado').innerHTML = estado;
+        document.getElementById('episodio').innerHTML = episodio.join('<br><br>');
+        
         document.getElementById('especies').innerHTML = especies;
-        document.getElementById('episodio').innerHTML = episodio;
         //document.getElementById('imagen').innerHTML = img;
         //document.getElementById('creado').innerHTML = creado;
         
@@ -47,3 +61,9 @@ const infoComic = async() => {
 }
 
 infoComic();
+
+/**let animales = ["gato", "perro", "vaca"];
+
+for (animal in animales){
+    console.log(animal);
+}*/
